4fcfbc28beb6f981db82b1f963173507205ddee6
WiktorMarcin <w-marcinkowski@wp.pl>
2024-10-17 22:31:16 +0200

    Added random color when creating squares. Deleted hover efect. Squares now got alpha channel. Every move over the squares makes it more visible by alpha

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const numberGridInput = document.querySelector("#numberGridInput");
 
 let gridSize = 16;
 
+function mathRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function colorRandom(){
+    return `RGBA( ${mathRandom(0,255)}, ${mathRandom(0,255)}, ${mathRandom(0,255)}, 0)`;
+}
+
+console.log(colorRandom());
+
 numberGridButton.addEventListener("click", () => {
     gridSize = numberGridInput.value;
 
@@ -21,11 +31,35 @@ function createSquare(){
         square.classList.add("square");
         square.style.width = 800 / gridSize + "px";
         square.style.height = 800 / gridSize + "px";
+        square.style.background = colorRandom();
     
         containerGrids.appendChild(square);
     }
 
+    alphaSquare();
 
 }
 
 createSquare();
+
+
+function alphaSquare(){
+    const square = document.querySelectorAll(".square");
+
+    square.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            const color = getComputedStyle(square).getPropertyValue("background-color")
+    
+            const parts = color.match(/[\d.]+/g);
+    
+            let alpha = parseFloat(parts[3]);
+    
+            alpha += 0.1;
+    
+            parts[3] = alpha;
+    
+            square.style.backgroundColor = `rgba(${parts.join(',')})`;
+        })
+    })    
+}
+
